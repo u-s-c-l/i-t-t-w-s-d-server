1dbdd84624eed75b8f4965596e7b91cc25ae79a5
Noah Platton <noahplatton@gmail.com>
2022-03-14 12:02:28 +0000

    Get db connections working

--- api/server.js
+++ api/server.js
@@ -11,11 +11,6 @@ server.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-server.get("/users", async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
-});
-
 server.get("/users/:username", async (req, res) => {
   const user = await User.findByUsername(req.params.username);
   res.json(user);
