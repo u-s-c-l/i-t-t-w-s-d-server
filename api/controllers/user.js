33b3b96a13b89d946b38a4b3bdfa6c4a9d13ae41
Noah Platton <noahplatton@gmail.com>
2022-03-15 10:57:51 +0000

    Resolve conflict

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const User = require("../models/User");
 
-<<<<<<< HEAD
 async function index(req, res) {
   //returns an array
   try {
@@ -21,24 +20,6 @@ async function findByUsername(req, res) {
   } catch (err) {
     res.status(403).json(err.message);
   }
-=======
-async function index (req, res) { //returns an array
-    try {
-        const users = await User.all
-        res.status(200).json(users)
-    } catch(err) {
-        res.status(403).json(err.message);
-    }
-}
-
-async function findByUsername (req, res){ //returns an
-try{ 
-        const user = await User.findByUsername(req.params.username);
-        res.status(200).json(user);
-} catch(err) {
-        res.status(403).json(err.message);
-        }
->>>>>>> 1abe51178ca960a9d6c2a7248919e12fa2d7d75c
 }
 
 module.exports = { index, findByUsername };
