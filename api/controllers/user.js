97b237faaedbc5145376bf306bfb683ae18ddb31
Noah Platton <noahplatton@gmail.com>
2022-03-15 09:56:41 +0000

    Set up config for integration tests

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-async function index (req, res) { //returns an array
-    try {
-        const users = await User.all
-        res.status(200).json(users)
-    } catch(err) {
-        res.status(403).send({err})
-    }
+async function index(req, res) {
+  //returns an array
+  try {
+    const users = await User.all;
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(403).send({ err });
+  }
 }
 
-async function findByUsername (req, res){ //returns an object
-try{ 
-        const user = await User.findByUsername(req.params.username);
-        res.status(200).json(user);
-} catch(err) {
-        res.status(403).json(err.message);
-        }
+async function findByUsername(req, res) {
+  //returns an object
+  try {
+    const user = await User.findByUsername(req.params.username);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(403).json(err.message);
+  }
 }
 
-module.exports = { index, findByUsername }
+module.exports = { index, findByUsername };
