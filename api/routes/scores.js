6ad1a3d83b5e42ebf0516057db115e0800cf25b7
Noah Platton <noahplatton@gmail.com>
2022-03-16 12:34:47 +0000

    Add token verification to routes

--- api/routes/scores.js
+++ api/routes/scores.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const { verifyToken } = require("../middleware/auth");
+
 const userController = require("../controllers/score");
 
-router.get("/", userController.index);
-router.get("/username/:username", userController.findByUsername);
-router.get("/cat/:cat", userController.findByCategory);
-router.get("/username/:username/cat/:cat", userController.findByUsernameAndCat);
-router.get("/leadersboard", userController.returnLeadersBoard);
+router.get("/", verifyToken, userController.index);
+router.get("/username/:username", verifyToken, userController.findByUsername);
+router.get("/cat/:cat", verifyToken, userController.findByCategory);
+router.get(
+  "/username/:username/cat/:cat",
+  verifyToken,
+  userController.findByUsernameAndCat
+);
+router.get("/leadersboard", verifyToken, userController.returnLeadersBoard);
 //router.post('/cat', userController.upsert)
-router.delete("/username/:username", userController.destroy);
+router.delete("/username/:username", verifyToken, userController.destroy);
 
 module.exports = router;
