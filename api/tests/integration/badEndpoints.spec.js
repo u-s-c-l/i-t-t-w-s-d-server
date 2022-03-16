21f1e748c9397cea4ac75176b6bc090ece102b6f
Noah Platton <noahplatton@gmail.com>
2022-03-16 15:33:45 +0000

    Finish integration tests

--- api/tests/integration/badEndpoints.spec.js
+++ api/tests/integration/badEndpoints.spec.js
@@ -26,7 +26,7 @@ describe("user endpoints", () => {
       test("it should return 404 error if no users found", async () => {
         const res = await request(api)
           .get("/users")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -36,7 +36,7 @@ describe("user endpoints", () => {
       test("it should return 404 error if no user found", async () => {
         const res = await request(api)
           .get("/users/jalexxx")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -48,7 +48,7 @@ describe("user endpoints", () => {
       test("it should return 404 error if no users/scores found", async () => {
         const res = await request(api)
           .get("/scores")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -58,7 +58,7 @@ describe("user endpoints", () => {
       test("it returns 404 if no user found", async () => {
         const res = await request(api)
           .get("/scores/username/jalexxx")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -68,7 +68,7 @@ describe("user endpoints", () => {
       test("it returns 404 if no category found", async () => {
         const res = await request(api)
           .get("/scores/cat/movies")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -78,7 +78,7 @@ describe("user endpoints", () => {
       test("it returns 404 if no match for given username and category", async () => {
         const res = await request(api)
           .get("/scores/username/jalexxx/cat/movies")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
@@ -88,7 +88,7 @@ describe("user endpoints", () => {
       test("it returns 404 if no users found", async () => {
         const res = await request(api)
           .get("/scores/leadersboard")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
--- api/tests/integration/goodEndpoints.spec.js
+++ api/tests/integration/goodEndpoints.spec.js
@@ -25,19 +25,19 @@ describe("successful endpoints", () => {
   });
 
   describe("user", () => {
-    describe("authenticated index", () => {
+    describe("authenticated GET /", () => {
       test("it should return all users", async () => {
         console.log(token);
         const res = await request(api)
           .get("/users")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(5);
       });
     });
 
-    describe("unauthenticated index", () => {
+    describe("unauthenticated GET /", () => {
       test("it should return 403 in case of invalid token", async () => {
         const res = await request(api)
           .get("/users")
@@ -46,18 +46,18 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated findByUsername", () => {
+    describe("authenticated GET /:username", () => {
       test("it should retrieve user based on username", async () => {
         const res = await request(api)
           .get("/users/jalexxx")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body.username).toEqual("jalexxx");
       });
     });
 
-    describe("unauthenticated findByUsername", () => {
+    describe("unauthenticated GET /:username", () => {
       test("it should return 403 if invalid token sent", async () => {
         const res = await request(api)
           .get("/users/jalexxx")
@@ -68,18 +68,18 @@ describe("successful endpoints", () => {
   });
 
   describe("score", () => {
-    describe("authenticated index", () => {
+    describe("authenticated GET /", () => {
       test("it should retrieve all users scores", async () => {
         const res = await request(api)
           .get("/scores")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(6);
       });
     });
 
-    describe("unauthenticated index", () => {
+    describe("unauthenticated GET /", () => {
       test("it should throw 403 error if invalid token sent", async () => {
         const res = await request(api)
           .get("/scores")
@@ -88,11 +88,11 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated findByUsername", () => {
+    describe("authenticated GET /username/:username", () => {
       test("it should retrieve all cat data for user", async () => {
         const res = await request(api)
           .get("/scores/username/jalexxx")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(2);
@@ -101,13 +101,13 @@ describe("successful endpoints", () => {
       test("it should return 404 if no user", async () => {
         const res = await request(api)
           .get("/scores/username/tester")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
     });
 
-    describe("unauthenticated findByUsername", () => {
+    describe("unauthenticated GET /username/:username", () => {
       test("it returns 403 error if invalid token sent", async () => {
         const res = await request(api)
           .get("/scores/username/jalexxx")
@@ -116,11 +116,11 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated findByCategory", () => {
+    describe("authenticated GET /cat/:cat", () => {
       test("it should retrieve all scores for a category", async () => {
         const res = await request(api)
           .get("/scores/cat/movies")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(2);
@@ -129,7 +129,7 @@ describe("successful endpoints", () => {
       test("it should retrieve all scores for a category", async () => {
         const res = await request(api)
           .get("/scores/cat/physics")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(1);
@@ -138,13 +138,13 @@ describe("successful endpoints", () => {
       test("it should return 404 if no category", async () => {
         const res = await request(api)
           .get("/scores/cat/test")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
     });
 
-    describe("unauthenticated findByCategory", () => {
+    describe("unauthenticated GET /cat/:cat", () => {
       test("it returns 403 error if invalid token sent", async () => {
         const res = await request(api)
           .get("/scores/cat/test")
@@ -153,11 +153,11 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated findByUsernameAndCat", () => {
+    describe("authenticated GET /username/:username/cat/:cat", () => {
       test("it should retrieve score for given username and category", async () => {
         const res = await request(api)
           .get("/scores/username/jalexxx/cat/movies")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body.score).toEqual(18);
@@ -166,7 +166,7 @@ describe("successful endpoints", () => {
       test("it should retrieve score for given username and category", async () => {
         const res = await request(api)
           .get("/scores/username/gi-ba-bu/cat/animals")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body.score).toEqual(23);
@@ -175,13 +175,13 @@ describe("successful endpoints", () => {
       test("it should return 404 if no score for given pair", async () => {
         const res = await request(api)
           .get("/scores/username/gi-ba-bu/cat/movies")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(404);
       });
     });
 
-    describe("unauthenticated findByUsernameAndCat", () => {
+    describe("unauthenticated GET /username/:username/cat/:cat", () => {
       test("should return 403 error if invalid token sent", async () => {
         const res = await request(api)
           .get("/scores/username/gi-ba-bu/cat/movies")
@@ -190,18 +190,18 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated returnLeadersBoard", () => {
+    describe("authenticated GET /leadersboard", () => {
       test("it retrieves array of highest scores", async () => {
         const res = await request(api)
           .get("/scores/leadersboard")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveLength(4);
       });
     });
 
-    describe("unauthenticated returnLeadersBoard", () => {
+    describe("unauthenticated GET /leadersboard", () => {
       test("it retrieves array of highest scores", async () => {
         const res = await request(api)
           .get("/scores/leadersboard")
@@ -210,18 +210,61 @@ describe("successful endpoints", () => {
       });
     });
 
-    describe("authenticated destroy", () => {
+    describe("authenticated POST /post", () => {
+      test("it returns 201 in case of success and updates score db", async () => {
+        const res = await request(api)
+          .post("/scores/post")
+          .send({
+            username: "tester",
+            cat: "movies",
+            score: 1,
+          })
+          .set("authorization", token);
+        expect(res.statusCode).toEqual(201);
+        const newRes = await request(api)
+          .get("/username/tester")
+          .set("authorization", token);
+        expect(newRes.body).toBeTruthy();
+      });
+
+      test("it returns 500 in case of error", async () => {
+        const res = await request(api)
+          .post("/scores/post")
+          .send({
+            username: "tester",
+            cat: "movies",
+          })
+          .set("authorization", token);
+        expect(res.statusCode).toEqual(500);
+      });
+    });
+
+    describe("unauthenticated POST /post", () => {
+      test("it returns 403 error if invalid token", async () => {
+        const res = await request(api)
+          .post("/scores/post")
+          .send({
+            username: "tester",
+            cat: "movies",
+            score: 1,
+          })
+          .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+        expect(res.statusCode).toEqual(403);
+      });
+    });
+
+    describe("authenticated DELETE /username/:username", () => {
       test("it destroys a users data", async () => {
         const res = await request(api)
           .delete("/scores/username/nplatton")
           .set("Accept", "application/json")
-          .set("authorization", `${token}`)
+          .set("authorization", token)
           .set("Accept", "application/json");
         expect(res.statusCode).toEqual(204);
       });
     });
 
-    describe("unauthenticated destroy", () => {
+    describe("unauthenticated DELETE /username/:username", () => {
       test("it won't destroy a users data if bad token given", async () => {
         const res = await request(api)
           .delete("/scores/username/nplatton")
@@ -233,26 +276,31 @@ describe("successful endpoints", () => {
   });
 
   describe("auth", () => {
-    describe("login", () => {
-      test("it ", async () => {
+    describe("POST /login", () => {
+      test("it retrieves object containing success=true and a token", async () => {
         const res = await request(api)
           .post("/auth/login")
           .send(JSON.stringify({ username: "jalexxx", password: "jalexxx" }))
           .set("Content-Type", "application/json");
         expect(res.statusCode).toEqual(200);
         expect(res.body.success).toBe(true);
+        expect(res.body.token).toBeTruthy();
       });
     });
 
-    describe("register", () => {
-      test("it ", async () => {
+    describe("POST /register", () => {
+      test("it adds a user to the db", async () => {
         const res = await request(api)
           .post("/auth/register")
-          .send(JSON.stringify({ username: "jalexxx", password: "jalexxx" }))
+          .send(JSON.stringify({ username: "tester", password: "tester" }))
           .set("Content-Type", "application/json");
         expect(res.statusCode).toEqual(201);
         expect(res.body.msg).toMatch(/user created/i);
-      });
+        const newRes = await request(api)
+          .get("/users/tester")
+          .set("authorization", token);
+        expect(newRes.body).toBeTruthy();
+      }, 100000);
     });
   });
 });
