d732637c20388f85348339bcc66226b005c53bad
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-03-18 18:41:14 +0100

    socket cors

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3004",
+    origin: /netlify/,
     methods: ["GET", "POST"],
     allowedHeaders: [],
     credentials: true,
